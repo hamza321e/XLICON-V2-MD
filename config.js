c2005b3d9e361bdd4fc7c31d4007045cbaa14908
hamza321e <163037294+hamza321e@users.noreply.github.com>
2024-03-19 20:17:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ global.owner = [
 
 //global.pairingNumber = "923207614149" //put your bot number here
 
-global.mods = ['923207614149','923417680053'] 
-global.prems = ['923207614149','923417680053']
-global.allowed = ['923207614149','923417680053']
+global.mods = ['923207614149','923229390827'] 
+global.prems = ['923207614149','923229390827']
+global.allowed = ['923207614149','923229390827']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
